b552a134a2a1c63153d79395a838000324373366
Chanpjk <adolasoom@gmail.com>
2025-08-26 14:32:17 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-  document.getElementById("btn").addEventListener("click",()=>{
-    document.getElementById("msg").classList.toggle("hidden");
-  });
-});
