978d7ea60ac0c43508b3adccedb7bd348d49492a
Chanpjk <adolasoom@gmail.com>
2025-08-26 14:32:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",()=>{
+  document.getElementById("btn").addEventListener("click",()=>{
+    document.getElementById("msg").classList.toggle("hidden");
+  });
+});
